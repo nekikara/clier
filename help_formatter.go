d35c0e06ae4eddfc620513c75c063ac61a763d9c
nekikara <a.nek1427@gmail.com>
2015-10-08 02:17:05 +0900

    Update NewHelpFormatter method

--- help_formatter.go
+++ help_formatter.go
@@ -1,26 +1,41 @@
 package flagparse
 
+import (
+	"math"
+	"regexp"
+)
+
 // HelpFormatter for generating usage messages and argument help strings.
 type HelpFormatter struct {
 	prog              string
 	indentIncrement   int
 	maxHelpPosition   int
-	width             string
+	width             int
 	currentIndent     int
 	level             int
 	actionMaxLength   int
-	rootSection       string
-	currentSection    string
-	whitespaceMatcher string
-	longBreakMatcher  string
+	rootSection       *section
+	currentSection    *section
+	whitespaceMatcher *regexp.Regexp
+	longBreakMatcher  *regexp.Regexp
 }
+
+type section struct{}
 
 // NewHelpFormatter returns new HelpFormatter struct.
 func NewHelpFormatter(prog string, indentIncrement, maxHelpPosition, width int) HelpFormatter {
 	hf := HelpFormatter{}
 	hf.prog = prog
 	hf.indentIncrement = indentIncrement
-	//hf.maxHelpPosition = math.Min(maxHelpPosition, math.Max(width-20, indentIncrement*2))
+	hf.maxHelpPosition = int(math.Min(float64(maxHelpPosition), math.Max(float64(width-20), float64(indentIncrement*2))))
+	hf.width = width
+	hf.currentIndent = 0
+	hf.level = 0
+	hf.actionMaxLength = 0
+	hf.rootSection = &section{}
+	hf.currentSection = hf.rootSection
+	hf.whitespaceMatcher = regexp.MustCompile(`\s+`)
+	hf.longBreakMatcher = regexp.MustCompile(`\n\n\n+`)
 
 	return hf
 }
