a97ff6caccf74605c7d38c18493d454c882109ce
nekikara <a.nek1427@gmail.com>
2015-10-10 00:08:16 +0900

    Display a usage message without no args.

--- flagparse.go
+++ flagparse.go
@@ -1,1 +1,29 @@
 package flagparse
+
+import (
+	"fmt"
+	"os"
+	"path"
+)
+
+// ArgumentParser is a struct for parsing command line strings
+type ArgumentParser struct {
+	prog string
+}
+
+// NewArgumentParser returns new ArgumentParser pointer
+func NewArgumentParser(prog string) *ArgumentParser {
+	ap := &ArgumentParser{}
+	if prog == "" {
+		prog = path.Base(os.Args[0])
+	}
+	ap.prog = prog
+	return ap
+}
+
+func (ap *ArgumentParser) ParseArgs(args, namespace string) {
+	fmt.Printf("usage: %s [-h]\n", ap.prog)
+	fmt.Println(`
+optional arguments:
+  -h, --help  show this help message and exit`)
+}
